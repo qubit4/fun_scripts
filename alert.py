0433df91f8c5c77dfe69721b772f88287e8aed48
intotheintuition <82866970+intotheintuition@users.noreply.github.com>
2021-10-10 16:35:28 +0300

    Update alert.py

--- alert.py
+++ alert.py
@@ -1,17 +1,17 @@
 import tkinter
 import tkinter.messagebox
 
-# Let's create the Tkinter window
+# creating the Tkinter window
 window = tkinter.Tk()
 window.title("Antivirus Response")
 
-# Let's create an alert box with 'messagebox' function
+# creating an alert box with 'messagebox' function
 tkinter.messagebox.showinfo("Alert Message", "Antivirus identified dangerous malware. Action needed.")
 
-# Let's also create a question for the user and based upon the response [Yes or No Question] display a message.
+# creating a question for the user and based upon the response display a message.
 response = tkinter.messagebox.askquestion("Antivirus Response", "Do you want to delete Operation System?")
 
-# A basic 'if/else' block where if user clicks on 'Yes' then it returns 1 else it returns 0. For each response you will display a message with the help of 'Label' method.
+# a basic 'if/else' block where if user clicks on 'Yes' then it returns 1 else it returns 0. For each response you will display a message with the help of 'Label' method.
 if response == 0:
     tkinter.Label(window, text = "Operation System will be deleted in 20 seconds").pack()
 else:
