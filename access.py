7fec4788fb5c11aacc54b73d888adb67f911d057
intotheintuition <82866970+intotheintuition@users.noreply.github.com>
2021-10-10 16:31:36 +0300

    Create access.py

---
+++ access.py
@@ -0,0 +1,12 @@
+while True:
+    print("What is your name?")
+    name = input()
+    if name != "Joe":
+    	continue
+    if name == "Joe":
+    	print("What is your password")
+    	password = input()
+    	if password == "swordfish":
+    		break
+
+print("access granted")
