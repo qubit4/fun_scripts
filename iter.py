f3cc46a205b53ee9718c7370e799e64c40d3d3c0
intotheintuition <82866970+intotheintuition@users.noreply.github.com>
2021-10-16 17:09:41 +0300

    Add files via upload

---
+++ iter.py
@@ -0,0 +1,20 @@
+# itertools
+
+import itertools
+import operator
+
+data = {1,2,3,4,5}
+
+result = itertools.accumulate(data, operator.mul)
+
+for each in result:
+    print(each)
+    
+
+## error operator not defined
+
+
+### jelly ,U$yJ}ị
+
+
+
