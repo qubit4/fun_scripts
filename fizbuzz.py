3d54ad2961627cff48772bb009683a55e240a1e5
qubit4 <82866970+qubit4@users.noreply.github.com>
2021-12-22 21:29:19 +0200

    Update fizbuzz.py

--- fizbuzz.py
+++ fizbuzz.py
@@ -1,17 +1,16 @@
-number = int(input("enter number: "))
-
-
 def fizzbuzz(number):
-    if number % 5 == 0 and number % 3 == 0:
-        print("fizzbuzz")
-    elif number % 5 == 0:
-        print("fizz")
-    elif number % 3 == 0:
-        print("buzz")
-    else:
-        print(number)
+    if (number % 5 == 0) and (number % 3 == 0):
+        return "fizzbuzz"
+    if number % 5 == 0:
+        return "fizz"
+    if number % 3 == 0:
+        return "buzz"
+    return number
 
 
 while True:
-    fizzbuzz(number)
-    number = int(input("enter number: "))
+    try:
+        number = int(input("enter number: "))
+        print(fizzbuzz(number))
+    except ValueError:
+        print("not a number")
