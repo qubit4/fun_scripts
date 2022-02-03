702c1047f5eb3d2ac8e192935bd55ada19a313f2
qubit4 <82866970+qubit4@users.noreply.github.com>
2022-02-03 12:52:56 +0100

    Add files via upload

---
+++ this_message_will_self-destruct.py
@@ -0,0 +1,45 @@
+import time
+import requests
+from os import remove
+from sys import argv
+
+
+def countdown(count):
+
+    while count:
+        mins, secs = divmod(count, 60)
+        timeformat = "{:02d}:{:02d}".format(mins, secs)
+        print(timeformat, end="\r")
+        time.sleep(1)
+        count -= 1
+
+
+def get_mission():
+    # content of the mission
+    url = "https://www.boredapi.com/api/activity"
+    response = requests.get(url)
+    jsonresponse = response.json()
+    mission = jsonresponse["activity"]
+
+    # message
+    message1 = ("Welcome, Jim. We haven't heard from you for some time. \n"
+                "Your mission, should you choose to accept it, is: \n")
+
+    message2 = ("As always, should you or any of your IM Force be caught or killed, \n"
+                "the Secretary will disavow any knowledge of your actions. \n"
+                "This message will self-destruct in 10 seconds. \n")
+
+    print(message1)
+    print(mission, "\n")
+    print(message2)
+
+    # countdown until self-destruction
+    countdown(10)
+    # ANSI escape sequence clearing terminal
+    print("\033c")
+    print("\x1bc")
+    # script now self-deletes
+    remove(argv[0])
+
+
+get_mission()
