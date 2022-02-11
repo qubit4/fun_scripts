1aa93135f1ec1d7f156a5b850fe6dbd5d1b9cfe2
qubit4 <82866970+qubit4@users.noreply.github.com>
2022-02-11 16:33:00 +0100

    Update useless_facts_GUI.py

--- useless_facts_GUI.py
+++ useless_facts_GUI.py
@@ -2,23 +2,23 @@ import tkinter as tk
 from tkinter import *
 import requests
 
-
+# window specifications
 root = Tk()
-root.geometry("400x300")
-root.title("random useless fact")
-
+root.geometry("350x280")
+root.title("random_fact_generator")
 T = Text(root, height=10, width=40)
 
-l = Label(root, text="Fact")
-l.config(font=("Courier", 14))
-
+# data specifications
 api_url = "https://uselessfacts.jsph.pl//random.json?language=en"
 response = requests.get(api_url)
 data = response.json()
 fact = data["text"]
 
 
+# on clicking "New Fact" the window is cleared and new data fetched and printed
 def get_fact():
+    T.delete("1.0", tk.END)
+
     response = requests.get(api_url)
     if response.status_code == 200:
         data = response.json()
@@ -26,18 +26,15 @@ def get_fact():
     else:
         fact = "Failed to read data. Status code is : {}".format(
             response.status_code)
-    T.delete("1.0", tk.END)
+
     T.insert(tk.END, fact)
 
 
-b1 = Button(root, text="Next", command=get_fact)
+b1 = Button(root, text="New Fact", command=get_fact)
 b2 = Button(root, text="Exit", command=root.destroy)
 
-l.pack()
+
 T.pack()
 b1.pack()
 b2.pack()
-
-T.insert(tk.END, fact)
-
 tk.mainloop()
